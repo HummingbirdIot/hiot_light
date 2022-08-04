088b4b5f8301921b0688b98e6b7c3a40360e399b
hummingbirdiot <hummingbirdiot@example.com>
2022-08-04 10:52:03 +0800

    Enable led

--- lua/light.lua
+++ lua/light.lua
@@ -19,12 +19,32 @@ function lightGW.Stop()
   end
 end
 
+local function green_led_on(isOn)
+  if isOn then
+    os.execute('gpioset 1 9=1')
+  else
+    os.execute('gpioset 1 9=0')
+  end
+end
+
+local function red_led_on(isOn)
+  if isOn then
+    os.execute('gpioset 1 8=1')
+  else
+    os.execute('gpioset 1 8=0')
+  end
+end
+
 function lightGW.Start()
+  green_led_on(false)
+  red_led_on(true)
   light_upgrade.Run(lightGW.Stop)
   print(">>>> Start helium_gateway")
   if not os.execute("/etc/init.d/helium_gateway start") then
     print("fail to start helium gateway")
   end
+  red_led_on(false)
+  green_led_on(true)
 end
 
 if ... then
